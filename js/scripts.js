8f26b0b2f1a7ac566c7d57c90ef767c8d12924f0
greatempire <thegr8empires@gmail.com>
2024-01-27 18:56:57 +0530

    update in product

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const listHoursArray = document.body.querySelectorAll(".list-hours li");
   listHoursArray[new Date().getDay()].classList.add("today");
 });
+// Get all the video elements on the page
+var videos = document.getElementsByTagName("myvideo");
+
+// Define a function to pause all videos except the one that is playing
+function pauseOthers() {
+  // Loop through all the videos
+  for (var i = 0; i < myvideo.length; i++) {
+    // If the video is not the one that triggered the event, pause it
+    if (myvideo[i] != event.target) {
+      myvideo[i].pause();
+    }
+  }
+}
+
+// Add an event listener to each video element for the play event
+for (var i = 0; i < myvideo.length; i++) {
+  myvideo[i].addEventListener("play", pauseOthers);
+}
